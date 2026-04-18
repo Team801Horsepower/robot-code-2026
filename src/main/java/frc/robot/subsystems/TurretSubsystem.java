b84f2bf79090b7f4bcee54734cc5c912c8d7d0d5
TheNerdyFunnyComedian <hudsonjimenez@gmail.com>
2026-04-18 11:40:08 -0400

    chore(turret): import FieldConstants and FieldZone

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -23,6 +23,8 @@ import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.FieldConstants;
+import frc.robot.Constants.FieldZone;
 import edu.wpi.first.math.MathUtil;
 
 import java.util.Optional;
